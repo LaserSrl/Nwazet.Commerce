d0e8ac4a68796537ea328b67be797c2fd95c30fe
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2017-10-20 17:30:14 +0200

    WIP drivers. Comments before writing code

--- Drivers/TerritoryHierarchyPartDriver.cs
+++ Drivers/TerritoryHierarchyPartDriver.cs
@@ -27,12 +27,16 @@ namespace Nwazet.Commerce.Drivers {
         }
 
         protected override DriverResult Editor(TerritoryHierarchyPart part, dynamic shapeHelper) {
+            var shapes = new List<DriverResult>();
             //part.Id == 0: new item
             if (part.Id != 0) {
-                //let's also add a shape that allows managing the territories in the hierarchy
+                //TODO: if the part is fully configured
+                //add a shape that allows managing the territories in the hierarchy
             }
+            //add a shape for configuration of the part
+            //Some configuration options may be locked depending on the territories in the hierarchy
             //TODO
-            return null;
+            return Combined(shapes.ToArray());
         }
 
         protected override DriverResult Editor(TerritoryHierarchyPart part, IUpdateModel updater, dynamic shapeHelper) {
