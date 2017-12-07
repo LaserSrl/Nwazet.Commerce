c43db30d31a65c1be6a497217c920a29c68c929f
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2017-12-07 14:39:05 +0100

    started implementation for localization Feature for territories

---
+++ Features/TerritoriesLocalizationFeature.cs
@@ -0,0 +1,73 @@
+﻿using Orchard.Environment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Orchard.Environment.Extensions.Models;
+using Orchard.ContentManagement.MetaData;
+using Nwazet.Commerce.Models;
+
+namespace Nwazet.Commerce.Features {
+    public class TerritoriesLocalizationFeature : IFeatureEventHandler {
+
+        private readonly IContentDefinitionManager _contentDefinitionManager;
+
+        public TerritoriesLocalizationFeature(
+            IContentDefinitionManager contentDefinitionManager) {
+
+            _contentDefinitionManager = contentDefinitionManager;
+        }
+
+        public void Enabled(Feature feature) {
+            if (feature.Descriptor.Id == "Territories.Localization") {
+                // Look for all ContentTypes that have a TerritoryHierarrchyPart
+                var hierarchyTypes = _contentDefinitionManager
+                    .ListTypeDefinitions()
+                    .Where(ctd => ctd
+                        .Parts
+                        .Any(pa => pa
+                            .PartDefinition
+                            .Name
+                            .Equals(TerritoryHierarchyPart.PartName, StringComparison.InvariantCultureIgnoreCase)));
+                if (hierarchyTypes.Any(ht => ht
+                    .Parts
+                    .Any(ctpd => ctpd
+                        .PartDefinition
+                        .Name
+                        .Equals("LocalizationPart", StringComparison.InvariantCultureIgnoreCase)))) {
+                    // At least a HierarchyType is localizable, so we make all TerritoryTypes localizable
+                    var territoryTypes = _contentDefinitionManager
+                        .ListTypeDefinitions()
+                        .Where(ctd => ctd
+                            .Parts
+                            .Any(pa => pa
+                                .PartDefinition
+                                .Name
+                                .Equals(TerritoryPart.PartName, StringComparison.InvariantCultureIgnoreCase)));
+                    foreach (var territoryType in territoryTypes) {
+                        _contentDefinitionManager
+                            .AlterTypeDefinition(territoryType.Name, builder => builder
+                                .WithPart("LocalizationPart"));
+                    }
+                }
+            }
+        }
+
+        #region Not used interface methods
+        public void Disabled(Feature feature) { }
+
+        public void Disabling(Feature feature) { }
+
+        public void Enabling(Feature feature) { }
+
+        public void Installed(Feature feature) { }
+
+        public void Installing(Feature feature) { }
+
+        public void Uninstalled(Feature feature) { }
+
+        public void Uninstalling(Feature feature) { }
+        #endregion
+    }
+}
---
+++ Settings/TerritoryLocalizationEditorEvents.cs
@@ -0,0 +1,176 @@
+﻿using Orchard.ContentManagement.MetaData;
+using Orchard.Environment.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Orchard.ContentManagement.MetaData.Builders;
+using Orchard.ContentTypes.Events;
+using Orchard.UI.Notify;
+using Orchard.ContentManagement.MetaData.Models;
+using Nwazet.Commerce.Models;
+using Orchard.Localization;
+
+namespace Nwazet.Commerce.Settings {
+    [OrchardFeature("Territories.Localization")]
+    public class TerritoryLocalizationEditorEvents 
+        : IContentDefinitionEventHandler {
+        // Call HierarchyType a ContentType that has a TerritoryHierarchyPart
+        // Call TerritoryType a ContentType that has a TerritoryPart
+        // If there are any HierarchyTypes that have a LocalizationPart, then all TerritoryTypes must have a LocalizationPart.
+        // This class enforces this situation.
+
+        private readonly IContentDefinitionManager _contentDefinitionManager;
+        private readonly INotifier _notifier;
+
+        public TerritoryLocalizationEditorEvents(
+            IContentDefinitionManager contentDefinitionManager,
+            INotifier notifier) {
+
+            _contentDefinitionManager = contentDefinitionManager;
+            _notifier = notifier;
+
+            T = NullLocalizer.Instance;
+        }
+        
+        private Localizer T;
+
+        public void ContentPartAttached(ContentPartAttachedContext context) {
+            // If we attached a LocalizationPart to a HierarchyType, and there are no other
+            // HierarchyTypes that have it, update all TerritoryTypes. (if there already are some
+            // we should have done that update already)
+            if ( // Added a LocalizationPart
+                context.ContentPartName.Equals("LocalizationPart", StringComparison.InvariantCultureIgnoreCase)
+                && // to a HierarchyType
+                HierarchyTypes.Any(ctd => ctd.Name.Equals(context.ContentTypeName)) 
+                && // and no other HierarchyType had a LocalizationPart already
+                !HierarchyTypes
+                    .Where(ctd => !ctd.Name.Equals(context.ContentTypeName))
+                    .Any(TypeHasLocalizationPart)) {
+                // Add LocalizationPart to all TerritoryTypes
+                foreach (var territoryType in TerritoryTypes) {
+                    AddLocalizationToType(territoryType);
+                }
+            }
+        }
+        
+        public void ContentPartDetached(ContentPartDetachedContext context) {
+            // If we detached a LocalizationPart from a TerritoryType, but there are HierarchyTypes
+            // that have it, attach it back
+            if (// We detached a LocalizationPart
+                context.ContentPartName.Equals("LocalizationPart", StringComparison.InvariantCultureIgnoreCase)
+                && // from a TerritoryType
+                TerritoryTypes.Any(ctd => ctd.Name.Equals(context.ContentTypeName))
+                && // but there are HierarchyTypes with a LocalizationPart
+                HierarchyTypes
+                    .Any(TypeHasLocalizationPart)) {
+                // Add the LocalizationPart back to the type
+                AddLocalizationToType(context.ContentTypeName);
+            }
+        }
+
+        public void ContentTypeCreated(ContentTypeCreatedContext context) {
+            // If we created a HierarchyType or a TerritoryType, there may be something that we have to do.
+            HandleNewType(context.ContentTypeDefinition);
+        }
+
+        public void ContentTypeImported(ContentTypeImportedContext context) {
+            // If we imported a HierarchyType or a TerritoryType, there may be something that we have to do.
+            HandleNewType(context.ContentTypeDefinition);
+        }
+        
+        private void HandleNewType(ContentTypeDefinition definition) {
+
+            if (// If it's a HierarchyType
+                TypeHasPart(definition, TerritoryHierarchyPart.PartName)
+                && // that has a LocalizationPart
+                TypeHasLocalizationPart(definition)
+                && // and no other HierarchyType had a LocalizationPart already
+                !HierarchyTypes
+                    .Where(ctd => !ctd.Name.Equals(definition.Name))
+                    .Any(TypeHasLocalizationPart)) {
+                // Add LocalizationPart to all TerritoryTypes
+                foreach (var territoryType in TerritoryTypes) {
+                    AddLocalizationToType(territoryType);
+                }
+            }
+            if (// If it's a TerritoryType
+                TypeHasPart(definition, TerritoryPart.PartName)
+                && // that does not have a LocalizationPart already
+                !TypeHasLocalizationPart(definition)
+                && // but there are HierarchyTypes with a LocalizationPart
+                HierarchyTypes
+                    .Any(TypeHasLocalizationPart)) {
+                // Add the LocalizationPart back to the type
+                AddLocalizationToType(definition);
+            }
+        }
+
+        private void AddLocalizationToType(string typeName) {
+            _contentDefinitionManager
+                .AlterTypeDefinition(typeName, builder => {
+                    builder.WithPart("LocalizationPart");
+                });
+            var name = _contentDefinitionManager.GetTypeDefinition(typeName).DisplayName;
+            name = string.IsNullOrWhiteSpace(name) ? typeName : name;
+            _notifier.Information(T("Contents of type \"{0}\" must have a LocalizationPart, so it was added.", name));
+        }
+
+        private void AddLocalizationToType(ContentTypeDefinition type) {
+            _contentDefinitionManager
+                .AlterTypeDefinition(type.Name, builder => {
+                    builder.WithPart("LocalizationPart");
+                });
+            var name = type.DisplayName;
+            name = string.IsNullOrWhiteSpace(name) ? type.Name : name;
+            _notifier.Information(T("Contents of type \"{0}\" must have a LocalizationPart, so it was added.", name));
+        }
+
+        private bool TypeHasLocalizationPart(ContentTypeDefinition type) {
+            return TypeHasPart(type, "LocalizationPart");
+        }
+
+        private bool TypeHasPart(ContentTypeDefinition type, string partName) {
+            return type
+                .Parts
+                .Any(ctpd => ctpd
+                    .PartDefinition
+                    .Name
+                    .Equals(partName, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        private IEnumerable<ContentTypeDefinition> HierarchyTypes {
+            get { return _contentDefinitionManager
+                    .ListTypeDefinitions()
+                    .Where(ctd => TypeHasPart(ctd, TerritoryHierarchyPart.PartName));
+            }
+        }
+
+        private IEnumerable<ContentTypeDefinition> TerritoryTypes {
+            get {
+                return _contentDefinitionManager
+                  .ListTypeDefinitions()
+                  .Where(ctd => TypeHasPart(ctd, TerritoryPart.PartName));
+            }
+        }
+
+        #region Not implemented IContentDefinitionEventHandler methods
+        public void ContentFieldAttached(ContentFieldAttachedContext context) { }
+
+        public void ContentFieldDetached(ContentFieldDetachedContext context) { }
+
+        public void ContentPartCreated(ContentPartCreatedContext context) { }
+
+        public void ContentTypeImporting(ContentTypeImportingContext context) { }
+
+        public void ContentTypeRemoved(ContentTypeRemovedContext context) { }
+
+        public void ContentPartRemoved(ContentPartRemovedContext context) { }
+
+        public void ContentPartImporting(ContentPartImportingContext context) { }
+
+        public void ContentPartImported(ContentPartImportedContext context) { }
+        #endregion
+    }
+}
