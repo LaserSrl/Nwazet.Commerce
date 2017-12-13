3e189ef0fcb41aca12f2de35e09392a7b79f1a88
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2017-12-13 14:17:13 +0100

    retrocompatible migrations in place, minus the fact that there are issues in the way shellDescriptor is updated (because it's not)

--- Migrations/BaseTaxImplementationsMigrations.cs
+++ Migrations/BaseTaxImplementationsMigrations.cs
@@ -1,58 +1,39 @@
 ﻿using Orchard.ContentManagement.MetaData;
-using Orchard.Data;
 using Orchard.Data.Migration;
 using Orchard.Environment.Extensions;
 using System;
-using System.Data;
 
 namespace Nwazet.Commerce.Migrations {
     [OrchardFeature("Nwazet.BaseTaxImplementations")]
     public class BaseTaxImplementationsMigrations : DataMigrationImpl {
 
-        private readonly ITransactionManager _transactionManager;
-
-        public BaseTaxImplementationsMigrations(
-            ITransactionManager transactionManager) {
-
-            _transactionManager = transactionManager;
-        }
-
         public int Create() {
             // we could be calling this because we enabled the feature in the migrations for 
             // Nwazet.Taxes. In that case, we do not have to actually create the tables, because
             // those were handled there in the past.
-            try {
 
+            try {
+                // If the table exists, this next method call succeeds. Otherwise, it throws an exception.
+                SchemaBuilder.ExecuteSql("SELECT * FROM Nwazet_Commerce_StateOrCountryTaxPartRecord");
+            } catch (Exception) {
+                // Table does not exist
                 SchemaBuilder.CreateTable("StateOrCountryTaxPartRecord", table => table
                     .ContentPartRecord()
                     .Column<string>("State")
                     .Column<string>("Country")
-                    .Column<double>("Rate")
+                    .Column<decimal>("Rate")
                     .Column<int>("Priority")
                 );
-
-                ContentDefinitionManager.AlterTypeDefinition("StateOrCountryTax", cfg => cfg
-                  .WithPart("StateOrCountryTaxPart"));
-
-                return 1;
-            } catch (Exception ex) {
-
-                //_transactionManager.RequireNew();
-                return 3;
             }
-        }
 
-        public int UpdateFrom1() {
+            ContentDefinitionManager.AlterTypeDefinition("StateOrCountryTax", cfg => cfg
+              .WithPart("StateOrCountryTaxPart"));
+
             ContentDefinitionManager.AlterTypeDefinition("ZipCodeTax", cfg => cfg
                 .WithPart("ZipCodeTaxPart"));
-            return 2;
+
+            return 1;
         }
 
-        public int UpdateFrom2() {
-            SchemaBuilder.AlterTable("StateOrCountryTaxPartRecord", table =>
-                table.AlterColumn("Rate", column =>
-                    column.WithType(DbType.Decimal)));
-            return 3;
-        }
     }
 }
--- Migrations/TaxMigrations.cs
+++ Migrations/TaxMigrations.cs
@@ -1,7 +1,5 @@
 ﻿using Orchard.ContentManagement.MetaData;
-using Orchard.Data;
 using Orchard.Data.Migration;
-using Orchard.Data.Migration.Records;
 using Orchard.Environment.Extensions;
 using Orchard.Modules.Services;
 using System.Data;
@@ -9,38 +7,20 @@ using System.Data;
 namespace Nwazet.Commerce.Migrations {
     [OrchardFeature("Nwazet.Taxes")]
     public class TaxMigrations : DataMigrationImpl {
-
-        private readonly IRepository<DataMigrationRecord> _dataMigrationRepository;
+        
         private readonly IModuleService _moduleService;
-        private readonly ITransactionManager _transactionManager;
 
         public TaxMigrations(
-            IRepository<DataMigrationRecord> dataMigrationRepository,
-            IModuleService moduleService,
-            ITransactionManager transactionManager) {
-
-            _dataMigrationRepository = dataMigrationRepository;
+            IModuleService moduleService) {
+            
             _moduleService = moduleService;
-            _transactionManager = transactionManager;
         }
 
         public int Create() {
             // Update: we moved the StateCountryTax and ZipCodeTax implementations to a separate feature.
             // Hence we need to skip the migrations steps related to them.
             return 4; 
-
-            //SchemaBuilder.CreateTable("StateOrCountryTaxPartRecord", table => table
-            //    .ContentPartRecord()
-            //    .Column<string>("State")
-            //    .Column<string>("Country")
-            //    .Column<double>("Rate")
-            //    .Column<int>("Priority")
-            //);
-
-            //ContentDefinitionManager.AlterTypeDefinition("StateOrCountryTax", cfg => cfg
-            //  .WithPart("StateOrCountryTaxPart"));
-
-            //return 1;
+            
         }
 
         public int UpdateFrom1() {
@@ -56,20 +36,15 @@ namespace Nwazet.Commerce.Migrations {
             return 3;
         }
 
-        //public int UpdateFrom3() {
-        //    // Everything that was done above now belongs to the Nwazet.BaseTaxImplementations feature.
-        //    // If we are running this method, it means we are updating an old pre-territories system.
-        //    // We need to activate the Nwazet.BaseTaxImplementations feature, and not run its migrations,
-        //    // because those would break the db by trying to recreate tables.
+        public int UpdateFrom3() {
+            // Everything that was done above now belongs to the Nwazet.BaseTaxImplementations feature.
+            // If we are running this method, it means we are updating an old pre-territories system.
+            // We need to activate the Nwazet.BaseTaxImplementations feature, and not run its migrations,
+            // because those would break the db by trying to recreate tables.
+            
+            _moduleService.EnableFeatures(new string[] { "Nwazet.BaseTaxImplementations" }, true);
 
-        //    _dataMigrationRepository.Create(new DataMigrationRecord {
-        //        Version = 3,
-        //        DataMigrationClass = "Nwazet.BaseTaxImplementations"
-        //    });
-
-        //    _moduleService.EnableFeatures(new string[] { "Nwazet.BaseTaxImplementations" }, true);
-
-        //    return 4;
-        //}
+            return 4;
+        }
     }
 }
