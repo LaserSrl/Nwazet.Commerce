2c45101437b3836d45227914187cb5f151aff6c7
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2019-02-26 17:41:41 +0100

    Started work on the feature for shipping method providers with a flexible range of configuration options

---
+++ Models/ApplicabilityCriterionRecord.cs
@@ -0,0 +1,19 @@
+﻿using Orchard.Environment.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nwazet.Commerce.Models {
+    [OrchardFeature("Nwazet.FlexibleShippingImplementations")]
+    public class ApplicabilityCriterionRecord {
+
+        public virtual int Id { get; set; } // Primary Key
+        public virtual string Description { get; set; }
+        public virtual string Type { get; set; }
+        public virtual string State { get; set; }
+
+        public virtual FlexibleShippingMethodRecord FlexibleShippingMethodRecord { get; set; }
+    }
+}
---
+++ Models/FlexibleShippingMethodRecord.cs
@@ -0,0 +1,20 @@
+﻿using Orchard.ContentManagement.Records;
+using Orchard.Environment.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nwazet.Commerce.Models {
+    [OrchardFeature("Nwazet.FlexibleShippingImplementations")]
+    public class FlexibleShippingMethodRecord : ContentPartRecord {
+        public virtual string Name { get; set; }
+        public virtual string ShippingCompany { get; set; }
+        public virtual string IncludedShippingAreas { get; set; }
+        public virtual string ExcludedShippingAreas { get; set; }
+        public virtual decimal DefatulPrice { get; set; }
+
+        public virtual IList<ApplicabilityCriterionRecord> ApplicabilityCriteria { get; set; }
+    }
+}
