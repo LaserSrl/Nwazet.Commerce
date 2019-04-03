ac4f6a1f3c40673c0f5925e023121b86c5b52f5f
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2019-04-03 18:02:30 +0200

    Added resource manifest to "export" scripts

---
+++ ResourceManifest.cs
@@ -0,0 +1,19 @@
+﻿using Orchard.UI.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nwazet.Commerce {
+    public class ResourceManifest : IResourceManifestProvider {
+
+        public void BuildManifests(ResourceManifestBuilder builder) {
+            var manifest = builder.Add();
+
+            manifest.DefineScript("Nwazet.iframe-transport").SetUrl("jquery.iframe-transport.js").SetDependencies("jQuery");
+            manifest.DefineScript("Nwazet.ShoppingCart")
+                .SetUrl("shoppingcart.min.js", "shoppingcart.js").SetDependencies("jQuery");
+        }
+    }
+}
