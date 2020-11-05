02158238555be2a413f810b6fc47fd3552c38c90
elena.lampugnani <elena.lampugnani@laser-group.com>
2020-11-05 18:22:50 +0100

    created service and method for performance of territories

---
+++ Services/ITerritoryPartRecordService.cs
@@ -0,0 +1,35 @@
+﻿using Nwazet.Commerce.Models;
+using Orchard;
+using System.Collections.Generic;
+
+namespace Nwazet.Commerce.Services {
+    public interface ITerritoryPartRecordService : IDependency {
+        /// <summary>
+        /// given the hierarchy returns the number of territories
+        /// </summary>
+        /// <param name="hierarchy"></param>
+        /// <returns></returns>
+        int GetHierarchyTerritoriesCount(TerritoryHierarchyPart hierarchy);
+
+        /// <summary>
+        /// given the hierarchy returns the list of territories
+        /// </summary>
+        /// <param name="hierarchy"></param>
+        /// <returns></returns>
+        List<TerritoryPartRecord> GetHierarchyTerritories(TerritoryHierarchyPart hierarchy);
+
+        /// <summary>
+        /// given the territory returns the number of child of territories
+        /// </summary>
+        /// <param name="territory"></param>
+        /// <returns></returns>
+        int GetTerritoriesChildCount(TerritoryPart territory);
+
+        /// <summary>
+        /// given the territory returns the list child of territories
+        /// </summary>
+        /// <param name="territory"></param>
+        /// <returns></returns>
+        List<TerritoryPartRecord> GetTerritoriesChild(TerritoryPart territory);
+    }
+}
---
+++ Services/TerritoryPartRecordService.cs
@@ -0,0 +1,48 @@
+﻿using Nwazet.Commerce.Models;
+using Orchard.Data;
+using Orchard.Environment.Extensions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Nwazet.Commerce.Services {
+    [OrchardFeature("Territories")]
+    public class TerritoryPartRecordService : ITerritoryPartRecordService {
+        private readonly IRepository<TerritoryPartRecord> _territoryPartRecord;
+
+        public TerritoryPartRecordService(
+            IRepository<TerritoryPartRecord> territoryPartRecord) {
+            _territoryPartRecord = territoryPartRecord;
+        }
+
+        // TODO : Join CommonVersionRecord? 
+        public List<TerritoryPartRecord> GetHierarchyTerritories(TerritoryHierarchyPart hierarchy) {
+            return _territoryPartRecord
+                .Table
+                .Where(tpr => tpr.Hierarchy.Id == hierarchy.Record.Id)
+                .ToList();
+        }
+
+        public int GetHierarchyTerritoriesCount(TerritoryHierarchyPart hierarchy) {
+            return _territoryPartRecord
+                .Table
+                .Where(tpr => tpr.Hierarchy.Id == hierarchy.Record.Id)
+                .Count();
+        }
+
+        // TODO : Join CommonVersionRecord? 
+        public List<TerritoryPartRecord> GetTerritoriesChild(TerritoryPart territory) {
+            return _territoryPartRecord
+                .Table
+                .Where(tpr => tpr.ParentTerritory.Id == territory.Record.Id)
+                .ToList();
+        }
+
+        public int GetTerritoriesChildCount(TerritoryPart territory) {
+            return _territoryPartRecord
+                .Table
+                .Where(tpr => tpr.ParentTerritory.Id == territory.Record.Id)
+                .Count();
+        }
+
+    }
+}
