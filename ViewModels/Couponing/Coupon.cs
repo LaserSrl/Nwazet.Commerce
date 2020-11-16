d2c03886b36b71ce7b006aa59ccc892bb8a3916f
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2020-11-16 16:58:10 +0100

    Coupon code validation

--- ViewModels/Couponing/Coupon.cs
+++ ViewModels/Couponing/Coupon.cs
@@ -23,6 +23,7 @@ namespace Nwazet.Commerce.ViewModels.Couponing {
         public string Name { get; set; } // Public Name of the coupon: e.g. Merry Christmas
 
         [StringLength(255),Required]
+        [RegularExpression(@"[a-zA-Z0-9]{1,255}")]
         public string Code { get; set; } // Actual code for the coupon: e.g. XMAS2020
 
         public bool Published { get; set; }
