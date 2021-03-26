054e3e987b2e59a679cab635b2dea49d635911af
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2021-03-26 12:16:26 +0100

    WIP upgrade coupons
    handling the configuration for providers

--- Services/Couponing/CouponApplicationService.cs
+++ Services/Couponing/CouponApplicationService.cs
@@ -93,8 +93,8 @@ namespace Nwazet.Commerce.Services.Couponing {
                         CouponRecord = context.Coupon
                     };
                     var descriptor = GetCriterion(criterion.Category, criterion.Type);
-                    // descriptor should exist
-                    if (descriptor == null) {
+                    // descriptor should exist and be enabled
+                    if (descriptor == null || !descriptor.IsAvailableForProcessing) {
                         continue;
                     }
                     descriptorsTest(descriptor, criterionContext);
@@ -118,8 +118,8 @@ namespace Nwazet.Commerce.Services.Couponing {
                 Dictionary<string, object> tokens = new Dictionary<string, object>();
                 foreach (var criterion in context.Coupon.LineCriteria) {
                     var descriptor = GetLineCriterion(criterion.Category, criterion.Type);
-                    // descriptor should exist
-                    if (descriptor == null) {
+                    // descriptor should exist and be enabled
+                    if (descriptor == null || !descriptor.IsAvailableForProcessing) {
                         continue;
                     }
                     // we need to test for each line. Note that this method, if the context
@@ -315,13 +315,12 @@ namespace Nwazet.Commerce.Services.Couponing {
             }
             context.ShoppingCart.PriceAlterations = allAlterations.OrderByDescending(cpa => cpa.Weight).ToList();
         }
-        
+
         #endregion
 
         #region Manage Applicability
-
-        public IEnumerable<TypeDescriptor<CouponApplicabilityCriterionDescriptor>>
-            DescribeApplicabilityCriteria() {
+        private IEnumerable<TypeDescriptor<CouponApplicabilityCriterionDescriptor>>
+            InnerDescribeApplicabilityCriteria() {
 
             var context = new DescribeCouponApplicabilityContext();
 
@@ -332,10 +331,25 @@ namespace Nwazet.Commerce.Services.Couponing {
             return context.Describe();
         }
 
+        public IEnumerable<TypeDescriptor<CouponApplicabilityCriterionDescriptor>>
+            DescribeApplicabilityCriteria() {
+
+            var fullSet = InnerDescribeApplicabilityCriteria();
+            var filteredSet = fullSet
+                .Select(td => new TypeDescriptor<CouponApplicabilityCriterionDescriptor>() {
+                    Category = td.Category,
+                    Name = td.Name,
+                    Description = td.Description,
+                    Descriptors = td.Descriptors.Where(cacd => cacd.IsAvailableForConfiguration)
+                })
+                .Where(td => td.Descriptors.Any());
+            return filteredSet;
+        }
+
         public CouponApplicabilityCriterionDescriptor
             GetCriterion(string category, string type) {
 
-            return DescribeApplicabilityCriteria()
+            return InnerDescribeApplicabilityCriteria()
                 .SelectMany(x => x.Descriptors)
                 .FirstOrDefault(c =>
                     c.Category == category
@@ -358,8 +372,8 @@ namespace Nwazet.Commerce.Services.Couponing {
 
         #region Manage Line Conditions
 
-        public IEnumerable<TypeDescriptor<CouponLineApplicabilityCriterionDescriptor>>
-            DescribeLineCriteria() {
+        private IEnumerable<TypeDescriptor<CouponLineApplicabilityCriterionDescriptor>>
+            InnerDescribeLineCriteria() {
 
             var context = new DescribeCouponLineApplicabilityContext();
 
@@ -370,10 +384,25 @@ namespace Nwazet.Commerce.Services.Couponing {
             return context.Describe();
         }
 
+        public IEnumerable<TypeDescriptor<CouponLineApplicabilityCriterionDescriptor>>
+            DescribeLineCriteria() {
+
+            var fullSet = InnerDescribeLineCriteria();
+            var filteredSet = fullSet
+                .Select(td => new TypeDescriptor<CouponLineApplicabilityCriterionDescriptor>() {
+                    Category = td.Category,
+                    Name = td.Name,
+                    Description = td.Description,
+                    Descriptors = td.Descriptors.Where(cacd => cacd.IsAvailableForConfiguration)
+                })
+                .Where(td => td.Descriptors.Any());
+            return filteredSet;
+        }
+
         public CouponLineApplicabilityCriterionDescriptor
             GetLineCriterion(string category, string type) {
 
-            return DescribeLineCriteria()
+            return InnerDescribeLineCriteria()
                 .SelectMany(x => x.Descriptors)
                 .FirstOrDefault(c =>
                     c.Category == category
