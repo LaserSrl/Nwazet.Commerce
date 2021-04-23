6c931be2ff507dcfa8ca1dce739b73a8ca23345f
elena.lampugnani <elena.lampugnani@laser-group.com>
2021-04-23 10:09:26 +0200

    add data-call-type logic

--- ResourceManifest.cs
+++ ResourceManifest.cs
@@ -14,7 +14,7 @@ namespace Nwazet.Commerce {
             manifest.DefineScript("Nwazet.iframe-transport")
                 .SetUrl("jquery.iframe-transport.min.js","jquery.iframe-transport.js").SetDependencies("jQuery"); //Duplicated: should we use Orchard.Resources version?
             manifest.DefineScript("Nwazet.ShoppingCart")
-                .SetUrl("shoppingcart.min.js?v=1.0", "shoppingcart.js?v=1.0").SetDependencies("jQuery");
+                .SetUrl("shoppingcart.min.js?v=1.1", "shoppingcart.js?v=1.1").SetDependencies("jQuery");
             manifest.DefineScript("Nwazet.CartSpinner")
                 .SetUrl("cartspinner.min.js?v=1.0", "cartspinner.js?v=1.0").SetDependencies("jQuery");
             manifest.DefineScript("Nwazet.AttributeExtensions")
