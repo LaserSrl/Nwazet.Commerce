7ae31e7b277a0a9ce7e89d03028cf928594f6f50
elena.lampugnani <elena.lampugnani@laser-group.com>
2021-06-17 15:31:31 +0200

    single non cumulative coupon

---
+++ ApplicabilityCriteria/Couponing/NonCumulativeStateCouponApplicabilityCriterion.cs
@@ -0,0 +1,62 @@
+﻿using Nwazet.Commerce.Descriptors.CouponApplicability;
+using Nwazet.Commerce.Services.Couponing;
+using Orchard;
+using Orchard.Caching;
+using Orchard.Environment.Extensions;
+using Orchard.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nwazet.Commerce.ApplicabilityCriteria.Couponing {
+    [OrchardFeature("Nwazet.Couponing")]
+    public class NonCumulativeStateCouponApplicabilityCriterion
+          : BaseCouponCriterionProvider, ICouponApplicabilityCriterionProvider {
+
+        public NonCumulativeStateCouponApplicabilityCriterion(
+           IWorkContextAccessor workContextAccessor,
+           ICacheManager cacheManager,
+           ISignals signals)
+           : base(workContextAccessor, cacheManager, signals) {
+
+        }
+        public override string ProviderName => "NonCumulativeStateCouponApplicabilityCriterion";
+
+        public override LocalizedString ProviderDisplayName => T("Criteria non cumulative state");
+
+
+        public void Describe(DescribeCouponApplicabilityContext describe) {
+            var isAvailableForConfiguration = IsAvailableForConfiguration();
+            var isAvailableForProcessing = IsAvailableForProcessing();
+            describe
+                .For("Coupon", T("Non cumulative state"), T("Non cumulative State"))
+                .Element("Non cumulative State",
+                    T("Non cumulative State"),
+                    T("If the criteria is present the coupon is not cumulative with other coupons"),
+                    (ctx) => ApplyCriteria(ctx, (b) => b, T("Coupon {0} cannot be cumulated.", ctx.CouponRecord.Code)),
+                    (ctx) => ApplyCriteria(ctx, (b) => b, T("Coupon {0} cannot be cumulated.", ctx.CouponRecord.Code)),
+                    (ctx) => T("Non cumulative State"),
+                    isAvailableForConfiguration, isAvailableForProcessing,
+                    null);
+        }
+
+        public void ApplyCriteria(CouponApplicabilityCriterionContext context,
+            Func<bool, bool> outerCriterion,
+            LocalizedString failureMessage) {
+            // Use outerCriterion to negate the test, so we can easily do
+            // true/false
+            if (context.IsApplicable) {
+                var result = outerCriterion(context.ApplicabilityContext.ShoppingCart?.PriceAlterations != null &&
+                    (!context.ApplicabilityContext.ShoppingCart.PriceAlterations.Any()));
+                //verify is current coupon
+                if (!result) {
+                    context.ApplicabilityContext.Message = failureMessage;
+                }
+                context.IsApplicable = result;
+                context.ApplicabilityContext.IsApplicable = result;
+            }
+        }
+    }
+}
