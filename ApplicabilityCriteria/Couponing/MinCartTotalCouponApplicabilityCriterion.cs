bc823dfd811e36c75f5822fefcc94e5bc35183ad
elena.lampugnani <elena.lampugnani@laser-group.com>
2021-07-13 09:45:53 +0200

    added currency

--- ApplicabilityCriteria/Couponing/MinCartTotalCouponApplicabilityCriterion.cs
+++ ApplicabilityCriteria/Couponing/MinCartTotalCouponApplicabilityCriterion.cs
@@ -1,8 +1,11 @@
 ﻿using Nwazet.Commerce.Descriptors.CouponApplicability;
 using Nwazet.Commerce.Filters;
+using Nwazet.Commerce.Models;
+using Nwazet.Commerce.Services;
 using Nwazet.Commerce.Services.Couponing;
 using Orchard;
 using Orchard.Caching;
+using Orchard.ContentManagement;
 using Orchard.Environment.Extensions;
 using Orchard.Localization;
 using System.Linq;
@@ -28,18 +31,30 @@ namespace Nwazet.Commerce.ApplicabilityCriteria.Couponing {
         public void Describe(DescribeCouponApplicabilityContext describe) {
             var isAvailableForConfiguration = IsAvailableForConfiguration();
             var isAvailableForProcessing = IsAvailableForProcessing();
+
+            var symbolCurrecy = GetCurrency();
+
             describe
                 .For("Cart", T("Total Cart"), T("Total Cart"))
                 .Element("The cart total must be at least",
-                    T("The cart total must be at least"),
-                    T("The cart total must be at least"),
+                    T("The cart total must be at least {0}", symbolCurrecy),
+                    T("The cart total must be at least {0}", symbolCurrecy),
                     (ctx) => PostCanBeAdded(ctx),
                     (ctx) => PostCanBeProcessed(ctx),
-                    (ctx) => T("The cart total must be at least {0} VALUTA", (decimal)ctx.State.Value),
+                    (ctx) => T("The cart total must be at least {0} {1}",
+                        symbolCurrecy,
+                        (decimal)ctx.State.Value),
                     isAvailableForConfiguration, isAvailableForProcessing,
                     MinCartTotalForm.FormName);
         }
 
+        private string GetCurrency() {
+            var currencyCode = _workContextAccessor.GetContext()
+                .CurrentSite.As<ECommerceCurrencySiteSettingsPart>().CurrencyCode;
+
+            var currency = Currency.Currencies[currencyCode];
+            return currency.Symbol;
+        }
 
         private void PostCanBeAdded(CouponPostApplicabilityContext context) {
             if (context.IsApplicable) {
@@ -57,7 +72,7 @@ namespace Nwazet.Commerce.ApplicabilityCriteria.Couponing {
             CouponPostApplicabilityContext context) {
 
             var minSubtotal = (decimal)context.State.Value;
-            context.IsApplicable &= !(context.BaseCartSubtotal
+            context.IsApplicable &= (context.BaseCartSubtotal
                 // Values are negative already
                 + context.CouponValues
                     .Where(c=>c.Coupon!=context.Coupon)
