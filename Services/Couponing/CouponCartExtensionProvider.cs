a40c96784a08cd3c95e3c44f273d242be5704713
elena.lampugnani <elena.lampugnani@laser-group.com>
2021-07-14 09:12:27 +0200

    added setting for coupon form visibility

--- Services/Couponing/CouponCartExtensionProvider.cs
+++ Services/Couponing/CouponCartExtensionProvider.cs
@@ -1,5 +1,7 @@
 ﻿using Nwazet.Commerce.Extensions;
 using Nwazet.Commerce.Models;
+using Orchard;
+using Orchard.ContentManagement;
 using Orchard.DisplayManagement;
 using Orchard.Environment.Extensions;
 using System;
@@ -14,24 +16,34 @@ namespace Nwazet.Commerce.Services.Couponing {
 
         private readonly IShoppingCart _shoppingCart;
         private readonly dynamic _shapeFactory;
+        private readonly IWorkContextAccessor _workContextAccessor;
+
 
         public CouponCartExtensionProvider(
             IShoppingCart shoppingCart,
-            IShapeFactory shapeFactory) {
+            IShapeFactory shapeFactory,
+            IWorkContextAccessor workContextAccessor) {
 
             _shoppingCart = shoppingCart;
             _shapeFactory = shapeFactory;
+            _workContextAccessor = workContextAccessor;
         }
 
         public IEnumerable<dynamic> CartExtensionShapes() {
-            var appliedCoupons = _shoppingCart
-                ?.PriceAlterations
-                ?.Where(cpa => CouponingUtilities.CouponAlterationType
-                    .Equals(cpa.AlterationType, StringComparison.InvariantCultureIgnoreCase))
-                ?? Enumerable.Empty<CartPriceAlteration>();
-            yield return _shapeFactory.CouponingCartExtension(AppliedCoupons: appliedCoupons);
-            //TODO: write on frontend a note for the customer congratulating them for 
-            // having active coupons, if any
+
+            var couponFormVisibility = _workContextAccessor.GetContext().CurrentSite
+                .As<CouponingSiteSettingPart>().CouponFormVisibility;
+
+            if (!couponFormVisibility) {
+                var appliedCoupons = _shoppingCart
+                    ?.PriceAlterations
+                    ?.Where(cpa => CouponingUtilities.CouponAlterationType
+                        .Equals(cpa.AlterationType, StringComparison.InvariantCultureIgnoreCase))
+                    ?? Enumerable.Empty<CartPriceAlteration>();
+                yield return _shapeFactory.CouponingCartExtension(AppliedCoupons: appliedCoupons);
+                    //TODO: write on frontend a note for the customer congratulating them for 
+                    // having active coupons, if any
+            }
         }
     }
 }
