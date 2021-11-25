facab584a3f4ba62bc808869f388f694cbd4d2ae
elena.lampugnani <elena.lampugnani@laser-group.com>
2021-11-25 17:47:37 +0100

    added viewmodel support

--- ViewModels/ProductAttributePartEditViewModel.cs
+++ ViewModels/ProductAttributePartEditViewModel.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 
 namespace Nwazet.Commerce.ViewModels {
     public class ProductAttributePartEditViewModel {
+        public ProductAttributePartEditViewModel() {
+            AttributeValueRecords = new List<ProductAttributeValueViewModel>();
+        }
+
         public IEnumerable<ProductAttributeValue> AttributeValues { get; set; }
         public int SortOrder { get; set; }
         public string DisplayName { get; set; }
@@ -11,6 +15,6 @@ namespace Nwazet.Commerce.ViewModels {
         public IEnumerable<IProductAttributeExtensionProvider> AttributeExtensionProviders { get; set; }
         public string CssName { get; set; }
         public string Meaning { get; set; }
-        public IList<ProductAttributeValueRecord> AttributeValueRecords { get; set; }
+        public IList<ProductAttributeValueViewModel> AttributeValueRecords { get; set; }
     }
 }
