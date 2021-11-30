ae2953a06e7a8d450b79c7f551bc79849a824928
elena.lampugnani <elena.lampugnani@laser-group.com>
2021-11-30 14:21:25 +0100

    fixed import export

--- Models/ProductAttributePart.cs
+++ Models/ProductAttributePart.cs
@@ -14,11 +14,26 @@ namespace Nwazet.Commerce.Models {
         {
             get
             {
-                return ProductAttributeValue.DeserializeAttributeValues(AttributeValuesString);
+                return this.Record?.AttributeValueRecords
+                    .Select(r => new ProductAttributeValue {
+                        Text = r.Text,
+                        ExtensionProvider = r.ExtensionProvider,
+                        IsLineAdjustment = r.IsLineAdjustment,
+                        PriceAdjustment = r.PriceAdjustment,
+                        SortOrder = r.SortOrder
+                    }) ?? Enumerable.Empty<ProductAttributeValue>();
             }
             set
             {
-                AttributeValuesString = ProductAttributeValue.SerializeAttributeValues(value);
+                Record.AttributeValueRecords = value
+                    .Select(r => new ProductAttributeValueRecord {
+                        Text = r.Text,
+                        ExtensionProvider = r.ExtensionProvider,
+                        IsLineAdjustment = r.IsLineAdjustment,
+                        PriceAdjustment = r.PriceAdjustment,
+                        SortOrder = r.SortOrder,
+                        AttributePartRecord = Record
+                    }).ToList();
             }
         }
 
@@ -54,10 +69,10 @@ namespace Nwazet.Commerce.Models {
             set { Store(r => r.Meaning, value); }
         }
 
-        public IList<ProductAttributeValueRecord> AttributeValueRecords {
-            get { return Record.AttributeValueRecords; }
-            set { Record.AttributeValueRecords = value; }
-        }
+        //public IList<ProductAttributeValueRecord> AttributeValueRecords {
+        //    get { return Retrieve(r => r.AttributeValueRecords); }
+        //    set { Store(r => r.AttributeValueRecords, value); }
+        //}
 
         internal string AttributeValuesString
         {
--- Models/ProductAttributeValueRecord.cs
+++ Models/ProductAttributeValueRecord.cs
@@ -11,6 +11,7 @@ namespace Nwazet.Commerce.Models {
     [OrchardFeature("Nwazet.Attributes")]
     public class ProductAttributeValueRecord {
         public virtual int Id { get; set; } //Primary Key
+        public virtual string GUIdentifier { get; set; }
         [Required, DisplayName("Value Name")]
         public virtual string Text { get; set; }
         [Required, DisplayName("Price Adjustment")]
@@ -21,6 +22,39 @@ namespace Nwazet.Commerce.Models {
         public virtual int SortOrder { get; set; }
         [DisplayName("Extension Provider")]
         public virtual string ExtensionProvider { get; set; }
-        public virtual ProductAttributePartRecord AttributePartRecord { get; set; } 
+        public virtual ProductAttributePartRecord AttributePartRecord { get; set; }
+
+        public static IEnumerable<ProductAttributeValueRecord> DeserializeAttributeValues(string attributeValues) {
+            if (attributeValues != null) {
+                return attributeValues.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(a => a.Split('=')).Select(av => {
+                        var attrSettings = av[1].Split(',');
+                        return new ProductAttributeValueRecord {
+                            Text = av[0],
+                            PriceAdjustment = Convert.ToDecimal(attrSettings[0]),
+                            IsLineAdjustment = Convert.ToBoolean(attrSettings[1]),
+                            // Check if sort order value is present, didn't exist in previous versions
+                            SortOrder = attrSettings.Length > 2 ? Convert.ToInt32(attrSettings[2]) : 0,
+                            // Check if extension provider value is present, didn't exist in previous versions
+                            ExtensionProvider = attrSettings.Length > 3 ? attrSettings[3] : string.Empty,
+                            GUIdentifier = attrSettings[4]
+                        };
+                    })
+                    .OrderBy(a => a.SortOrder)
+                    .ThenBy(a => a.Text)
+                    .ToList();
+            } else {
+                return new List<ProductAttributeValueRecord>();
+            }
+        }
+
+        public static string SerializeAttributeValues(IEnumerable<ProductAttributeValueRecord> attributeValues) {
+            if (attributeValues != null) {
+                return string.Join(";", attributeValues.Select(a => a.Text + "=" + a.PriceAdjustment + ","
+                    + a.IsLineAdjustment + "," + a.SortOrder + "," + a.ExtensionProvider + ","+a.GUIdentifier));
+            } else {
+                return string.Empty;
+            }
+        }
     }
 }
