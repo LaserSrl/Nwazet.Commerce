08be85c6828247490da8b3666c7fb5e238007bdd
elena.lampugnani <elena.lampugnani@laser-group.com>
2021-12-02 17:32:39 +0100

    new providers price and vatconfiguration

---
+++ Services/Combinations/PricePartDetailProvider.cs
@@ -0,0 +1,46 @@
+﻿using Nwazet.Commerce.Models;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.MetaData;
+using Orchard.ContentManagement.MetaData.Builders;
+using Orchard.Environment.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nwazet.Commerce.Services.Combinations {
+    [OrchardFeature("Nwazet.ProductCombinations")]
+    public class PricePartDetailProvider : 
+        BaseCombinationDetailProvider {
+        public PricePartDetailProvider(
+           IContentDefinitionManager contentDefinitionManager)
+           : base(contentDefinitionManager) {
+        }
+
+        public override ContentTypeDefinitionBuilder AlterCombinationDefinition(
+            ContentTypeDefinitionBuilder previous,
+            string containerTypeName) {
+
+            var containerDefinition = _contentDefinitionManager.GetTypeDefinition(containerTypeName);
+
+            if (containerDefinition != null
+                && containerDefinition.Parts.Any(ctpd => ctpd.PartDefinition.Name == "PricePart")) {
+                // alter the definition
+                return previous.WithPart("PricePart");
+            }
+            return previous;
+        }
+
+        public override void Synchronize(
+           CombinationContainerPart container, CombinationPart combination) {
+
+            var sourcePart = container.As<PricePart>();
+            var targetPart = combination.As<PricePart>();
+            if (sourcePart != null && targetPart != null) {
+                // Copy properties from the contaoiner to the combination.
+                targetPart.EffectiveUnitPrice = sourcePart.EffectiveUnitPrice;
+            }
+        }
+    }
+}
---
+++ Services/Combinations/VatConfigurationPartDetailProvider.cs
@@ -0,0 +1,34 @@
+﻿using Orchard.ContentManagement;
+using Orchard.ContentManagement.MetaData;
+using Orchard.ContentManagement.MetaData.Builders;
+using Orchard.Environment.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nwazet.Commerce.Services.Combinations {
+    [OrchardFeature("Nwazet.AdvancedVAT")]
+    public class VatConfigurationPartDetailProvider :
+        BaseCombinationDetailProvider {
+        public VatConfigurationPartDetailProvider(
+           IContentDefinitionManager contentDefinitionManager)
+           : base(contentDefinitionManager) {
+        }
+
+        public override ContentTypeDefinitionBuilder AlterCombinationDefinition(
+           ContentTypeDefinitionBuilder previous,
+           string containerTypeName) {
+
+            var containerDefinition = _contentDefinitionManager.GetTypeDefinition(containerTypeName);
+
+            if (containerDefinition != null
+                && containerDefinition.Parts.Any(ctpd => ctpd.PartDefinition.Name == "ProductVatConfigurationPart")) {
+                // alter the definition
+                return previous.WithPart("ProductVatConfigurationPart");
+            }
+            return previous;
+        }
+    }
+}
