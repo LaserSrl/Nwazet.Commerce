3eca2e5709d23bafbe45bdf54d2245bcb9258511
elena.lampugnani <elena.lampugnani@laser-group.com>
2021-12-03 17:39:42 +0100

    created shape empty in price and vat provider and added localization provider but the logic needs to be fixed

---
+++ Services/Combinations/LocalizationPartDetailProvider.cs
@@ -0,0 +1,49 @@
+﻿using Nwazet.Commerce.Models;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.MetaData;
+using Orchard.ContentManagement.MetaData.Builders;
+using Orchard.Environment.Extensions;
+using Orchard.Localization.Models;
+using System.Linq;
+
+namespace Nwazet.Commerce.Services.Combinations {
+    [OrchardFeature("Nwazet.ProductCombinations")]
+    public class LocalizationPartDetailProvider :
+        BaseCombinationDetailProvider {
+        public LocalizationPartDetailProvider(
+           IContentDefinitionManager contentDefinitionManager)
+           : base(contentDefinitionManager) {
+        }
+        public override ContentTypeDefinitionBuilder AlterCombinationDefinition(
+           ContentTypeDefinitionBuilder previous,
+           string containerTypeName) {
+
+            var containerDefinition = _contentDefinitionManager.GetTypeDefinition(containerTypeName);
+
+            if (containerDefinition != null
+                && containerDefinition.Parts.Any(ctpd => ctpd.PartDefinition.Name == "LocalizationPart")) {
+                // alter the definition
+                return previous.WithPart("LocalizationPart");
+            }
+            return previous;
+        }
+
+        public override void Synchronize(
+           CombinationContainerPart container, CombinationPart combination) {
+
+            var sourcePart = container.As<LocalizationPart>();
+            var targetPart = combination.As<LocalizationPart>();
+            if (sourcePart != null && targetPart != null) {
+                // Copy properties from the contaoiner to the combination.
+                targetPart.Culture = sourcePart.Culture;
+
+                //??
+                if (sourcePart.MasterContentItem == null) {
+                    targetPart.MasterContentItem = container.ContentItem;
+                } else {
+                    targetPart.MasterContentItem = sourcePart.MasterContentItem;
+                }
+            }
+        }
+    }
+}
