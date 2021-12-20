f1d69c32e59ce85547931414a4f0568923aec158
elena.lampugnani <elena.lampugnani@laser-group.com>
2021-12-20 17:54:42 +0100

    managed select new combination

---
+++ ViewModels/Combinations/CombinationAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Orchard.Environment.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nwazet.Commerce.ViewModels.Combinations {
+    [OrchardFeature("Nwazet.ProductCombinations")]
+    public class CombinationAttribute {
+        public int AttributeId { get; set; }
+        public string AttributeDisplayName { get; set; }
+        public List<AttributeValue> Values { get; set; }
+        public int SelectedAttributeValue { get; set; }
+    }
+
+    [OrchardFeature("Nwazet.ProductCombinations")]
+    public class AttributeValue {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+}
