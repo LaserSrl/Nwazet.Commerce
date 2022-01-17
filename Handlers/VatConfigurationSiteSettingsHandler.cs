180d4e3829d468ea1ecf4c81b2547c8bc7d38b58
Andrea Piovanelli <andrea.piovanelli@laser-group.com>
2022-01-17 14:33:00 +0100

    Added cache management in PreventDeletingDefaultVatConfigurationEventHandler and cache evict (new handler VatConfigurationSiteSettingsHandler).

---
+++ Handlers/VatConfigurationSiteSettingsHandler.cs
@@ -0,0 +1,34 @@
+﻿using Orchard.Caching;
+using Orchard.ContentManagement.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nwazet.Commerce.Models;
+
+namespace Nwazet.Commerce.Handlers {
+    class VatConfigurationSiteSettingsHandler : ContentHandler {
+        private readonly ISignals _signals;
+
+        public VatConfigurationSiteSettingsHandler(ISignals signals) {
+            _signals = signals;
+
+            // Evict cached content when updated, removed or destroyed.
+            OnUpdated<VatConfigurationSiteSettingsPart>(
+                (context, part) => Invalidate());
+            OnImported<VatConfigurationSiteSettingsPart>(
+                (context, part) => Invalidate());
+            OnPublished<VatConfigurationSiteSettingsPart>(
+                (context, part) => Invalidate());
+            OnRemoved<VatConfigurationSiteSettingsPart>(
+                (context, part) => Invalidate());
+            OnDestroyed<VatConfigurationSiteSettingsPart>(
+                (context, part) => Invalidate());
+        }
+
+        private void Invalidate() {
+            _signals.Trigger(VatConfigurationSiteSettingsPart.CacheKey);
+        }
+    }
+}
