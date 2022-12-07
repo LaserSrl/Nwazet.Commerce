9aea3bad0dd46bedaa9fe6fdb44c0e9da72954c3
Matteo Piovanelli <matteo.piovanelli@laser-group.com>
2022-12-07 11:21:46 +0100

    WIP added part. Added event bus events for order

---
+++ Drivers/InventoryControl/InventoryControlPartDriver.cs
@@ -0,0 +1,15 @@
+﻿using Nwazet.Commerce.Models;
+using Orchard.ContentManagement.Drivers;
+using Orchard.Environment.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nwazet.Commerce.Drivers.InventoryControl {
+    [OrchardFeature("Nwazet.InventoryControl")]
+    public class InventoryControlPartDriver 
+        : ContentPartCloningDriver<InventoryControlPart> {
+    }
+}
---
+++ Events/IOrderEventHandler.cs
@@ -0,0 +1,20 @@
+﻿using Nwazet.Commerce.Models;
+using Orchard.ContentManagement;
+using Orchard.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nwazet.Commerce.Events {
+    public interface IOrderEventHandler : IEventHandler {
+        void OnNewOrder(OrderPart order);
+        void OnNewPayment(OrderPart order);
+        void OnOrderError(OrderPart order, Dictionary<string,string> errors);
+
+        void OnOrderStatusChanged(OrderPart order, string previousStatus);
+        void OnOrderTrackingUrlChanged(OrderPart order);
+        void OnOrderStatusChangedProduct(OrderPart order, ContentItem product, string previousStatus);
+    }
+}
