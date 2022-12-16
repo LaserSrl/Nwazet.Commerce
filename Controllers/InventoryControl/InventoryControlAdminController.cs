304bcc23e92f9ebaac3c7cf189b203082df663cf
Matteo Piovanelli <matteo.piovanelli@laser-group.com>
2022-12-16 11:14:08 +0100

    Added some validation to actions to increase/decrease inventory

--- Controllers/InventoryControl/InventoryControlAdminController.cs
+++ Controllers/InventoryControl/InventoryControlAdminController.cs
@@ -10,6 +10,7 @@ using Orchard.UI.Admin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Mvc;
@@ -18,6 +19,9 @@ namespace Nwazet.Commerce.Controllers.InventoryControl {
     [OrchardFeature("Nwazet.InventoryControl")]
     [Admin]
     public class InventoryControlAdminController : Controller {
+        // The fact this controller is currently decorated as [Admin] means it will
+        // respond with the standard "Unauthorized" webpage to any request from user
+        // who lack the permission to access the backend.
         private readonly IProductInventoryService _productInventoryService;
         private readonly IAuthorizer _authorizer;
         private readonly IContentManager _contentManager;
@@ -38,14 +42,18 @@ namespace Nwazet.Commerce.Controllers.InventoryControl {
 
         [HttpPost]
         public ActionResult RemoveInventory(int id, int quantity = 1) {
-            
+
+            if (quantity < 1) {
+                return Unauthorized(T("Quantity not valid: must be greater than 0."));
+            }
             var product = _contentManager.Get<ProductPart>(id);
+            if (product == null) {
+                return Unauthorized(T("Product not valid."));
+            }
             if (!_authorizer.Authorize(
                 CommercePermissions.ManageProducts, product, T("Not authorized to manage products"))) {
-                return new HttpUnauthorizedResult();
+                return Unauthorized(T("Not authorized to manage products"));
             }
-            // TODO: product not null
-            // TODO: quantity must be > 0
             // TODO: bundles, combinations...
             _productInventoryService.UpdateInventory(product, -quantity);
             return new JsonResult {
@@ -59,13 +67,17 @@ namespace Nwazet.Commerce.Controllers.InventoryControl {
         [HttpPost]
         public ActionResult AddInventory(int id, int quantity = 1) {
 
+            if (quantity < 1) {
+                return Unauthorized(T("Quantity not valid: must be greater than 0."));
+            }
             var product = _contentManager.Get<ProductPart>(id);
+            if (product == null) {
+                return Unauthorized(T("Product not valid."));
+            }
             if (!_authorizer.Authorize(
                 CommercePermissions.ManageProducts, product, T("Not authorized to manage products"))) {
-                return new HttpUnauthorizedResult();
+                return Unauthorized(T("Not authorized to manage products"));
             }
-            // TODO: product not null
-            // TODO: quantity must be > 0
             // TODO: bundles, combinations...
             _productInventoryService.UpdateInventory(product, quantity);
             return new JsonResult {
@@ -75,5 +87,14 @@ namespace Nwazet.Commerce.Controllers.InventoryControl {
                 }
             };
         }
+
+        private JsonResult Unauthorized(LocalizedString msg) {
+            Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+            // prevent IIS 7.0 classic mode from handling the 401 itself
+            Response.SuppressFormsAuthenticationRedirect = true;
+            return new JsonResult {
+                Data = new { Message = msg.Text }
+            };
+        } 
     }
 }
